41d972d23202a5b23bca4633c180595ef0408662
matildasmeds <matilda.smeds@gmail.com>
2017-10-26 08:34:52 +0200

    rustlang book ch12
    Next: ch12-03-improving-error-handling-and-modularity.html#extracting-logic-from-main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,10 +2,15 @@ use std::env;
 use std::str::*;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args);
+    let config = config.unwrap_or_else(|err| {
+        println!("Error reading arguments: {}", err);
+        process::exit(1);
+    });
 
     let mut f = File::open(config.filename).expect("file not found");
 
@@ -22,11 +27,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
         let query = String::from_str(&args[1]).unwrap(); // another option would be clone
         let filename = String::from_str(&args[2]).unwrap();
         println!("Searching for {}", query);
         println!("In file {}", filename);
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
