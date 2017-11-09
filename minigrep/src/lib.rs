82ad45f676914e22432c62f4e0bab11be20ba12b
matildasmeds <matilda.smeds@gmail.com>
2017-11-09 08:16:25 +0100

    Applying Rust iterator adaptors :loop:

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -40,14 +40,9 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 #[cfg(test)]
